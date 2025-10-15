ac61f436e0135451fab46c48f299a3778dd3940c
PAAWSA02LC0909 <Duoc@PAAWSA02LC0909.duoc.acad>
2025-10-14 22:19:39 -0300

    Fix

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FooterPrincipal(){
+    return (
+        <div className='row mt-5 bg-success'>
+
+            <div className='col-lg-12 text-center pt-2 text-light'>
+                <p>Industrias Paralelpipedo Inc.</p>
+                <p>Av. Concha y toro 666, Puente Alto</p>
+                <a className='btn btn-secondary' href='https://wa.me/56982841602/?text=Hola ka ase'><i class="fa-brands fa-whatsapp"></i> Contactanos por WhatsApp</a>
+            </div>
+
+        </div>
+    )
+}
+
+export default FooterPrincipal;
